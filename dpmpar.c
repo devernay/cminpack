2361197b48ca8271880704592b3b3857e48a497f
devernay <devernay@1da6dca5-a024-0410-bc4f-be26d6ae0add>
2012-04-12 17:27:07 +0000

    add values as comment

--- dpmpar.c
+++ dpmpar.c
@@ -184,11 +184,11 @@ real dpmpar(int i)
 
     switch(i) {
         case 1:
-            return DPMPAR(real,EPSILON);
+            return DPMPAR(real,EPSILON); /* 2.2204460492503131e-16 | 1.19209290e-07F */
         case 2:
-            return DPMPAR(real,MIN);
+            return DPMPAR(real,MIN);    /* 2.2250738585072014e-308 | 1.17549435e-38F */
         default:
-            return DPMPAR(real,MAX);
+            return DPMPAR(real,MAX);    /* 1.7976931348623157e+308 | 3.40282347e+38F */
     }
 
 /*     Last card of function dpmpar. */
